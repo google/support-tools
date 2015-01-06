18cc0d72efa778fc1560537e182eb7b7a1511b84
Sverre Rabbelier <srabbelier@google.com>
2015-01-05 16:37:30 -0800

    Use the new issues module in bitbucket_issue_converter

--- googlecode-issues-exporter/bitbucket_issue_converter.py
+++ googlecode-issues-exporter/bitbucket_issue_converter.py
@@ -25,377 +25,136 @@ import argparse
 import json
 import sys
 
-
-class InvalidUserError(Exception):
-  """Error for an invalid user."""
+import issues
 
 
-class ProjectNotFoundError(Exception):
-  """Error for a non-existent project."""
-
-
-class GoogleCodeIssue(object):
-  """Google Code issue.
-
-  Handles parsing and viewing a Google Code issue.
+class UserService(issues.UserService):
+  """BitBucket user operations.
   """
 
-  def __init__(self, issue):
-    """Initialize the GoogleCodeIssue.
+  def IsUser(self, username):
+    """Returns wheter a username is a valid user.
 
-    Args:
-      issue: The Google Code Issue.
+    BitBucket does not have a user api, so accept all usernames.
     """
-    self._issue = issue
-
-  def GetOwner(self):
-    """Get the owner username of a Google Code issue.
-
-    Returns:
-      The Google Code username that owns the issue or the
-      repository owner if no mapping or email address exists.
-    """
-    if "owner" not in self._issue:
-      return self.GetAuthor()
-
-    return self._issue["owner"]["name"]
-
-  def GetContent(self):
-    """Get the content from a Google Code issue.
-
-    Returns:
-      The issue content
-    """
-    return self._issue["summary"]
-
-  def GetContentUpdatedOn(self):
-    """Get the date the content was last updated from a Google Code issue.
-
-    Returns:
-      The time stamp when the issue content was last updated
-    """
-    return self._issue["updated"]
-
-  def GetCreatedOn(self):
-    """Get the creation date from a Google Code issue.
-
-    Returns:
-      The time stamp when the issue content was created
-    """
-    return self._issue["published"]
-
-  def GetId(self):
-    """Get the id from a Google Code issue.
-
-    Returns:
-      The issue id
-    """
-    return self._issue["id"]
-
-  def GetKind(self):
-    """Get the kind from a Google Code issue.
-
-    Returns:
-      The issue kind, if none is found defaults to 'Defect'
-    """
-    types = [t for t in self._issue["labels"] if "Type-" in t]
-    if types:
-      return types[0][len("Type-"):]
-    return "Defect"
-
-  def GetPriority(self):
-    """Get the priority from a Google Code issue.
-
-    Returns:
-      The issue priority, if none is found defaults to 'Medium'
-    """
-    priorities = [p for p in self._issue["labels"] if "Priority-" in p]
-    if priorities:
-      return priorities[0][len("Priority-"):]
-    return "Medium"
-
-  def GetAuthor(self):
-    """Get the author's username of a Google Code issue.
-
-    Returns:
-      The Google Code username that the issue is authored by or the
-      repository owner if no mapping or email address exists.
-    """
-    if "author" not in self._issue:
-      return None
-
-    return self._issue["author"]["name"]
-
-  def GetStatus(self):
-    """Get the status from a Google Code issue.
-
-    Returns:
-      The issue status
-    """
-    status = self._issue["status"].lower()
-    if status == "accepted":
-      status = "open"
-    return status
-
-  def GetTitle(self):
-    """Get the title from a Google Code issue.
-
-    Returns:
-      The issue title
-    """
-    return self._issue["title"]
-
-  def GetUpdatedOn(self):
-    """Get the date the issue was last updated.
-
-    Returns:
-      The time stamp when the issue was last updated
-    """
-    return self.GetCreatedOn()
-
-  def GetComments(self):
-    """Get the list of comments for the issue (if any).
-
-    Returns:
-      The list of comments attached to the issue
-    """
-    return self._issue["items"]
+    return True
 
 
-class GoogleCodeComment(object):
-  """Google Code Comment.
+class IssueService(issues.IssueService):
+  """Abstract issue operations.
 
-  Handles parsing and viewing a Google Code Comment.
+  Handles creating and updating issues and comments on an user API.
   """
+  def __init__(self):
+    self._bitbucket_issues = []
+    self._bitbucket_comments = []
 
-  def __init__(self, googlecode_comment, issue_id):
-    """Initialize the GoogleCodeComment.
+  def GetIssues(self, state="open"):
+    """Gets all of the issue for the repository.
 
-    Args:
-      googlecode_comment: The Google Code Comment.
-      issue_id: The issue id
-    """
-    self._comment = googlecode_comment
-    self._issue_id = issue_id
-
-  def GetContent(self):
-    """Get the content from a Google Code comment.
-
-    Returns:
-      The issue comment
-    """
-    return self._comment["content"]
-
-  def GetCreatedOn(self):
-    """Get the creation date from a Google Code comment.
-
-    Returns:
-      The time stamp when the issue comment content was created
-    """
-    return self._comment["published"]
-
-  def GetId(self):
-    """Get the id from a Google Code comment.
-
-    Returns:
-      The issue comment id
-    """
-    return self._comment["id"]
-
-  def GetIssueId(self):
-    """Get the issue id from a Google Code comment.
-
-    Returns:
-      The issue id
-    """
-    return self._issue_id
-
-  def GetUpdatedOn(self):
-    """Get the date the issue comment content was last updated.
-
-    Returns:
-      The time stamp when the issue comment content was last updated
-    """
-    return self.GetCreatedOn()
-
-  def GetAuthor(self):
-    """Get the author's username of a Google Code issue comment.
-
-    Returns:
-      The Google Code username that the issue comment is authored by or the
-      repository owner if no mapping or email address exists.
-    """
-    if "author" not in self._comment:
-      return None
-
-    return self._comment["author"]["name"]
-
-
-class BitBucketIssue(object):
-  """BitBucket issue.
-
-  Handles creating and viewing a BitBucket issue.
-  """
-
-  def __init__(self, googlecode_issue):
-    """Initialize the BitBucketIssue from a GoogleCodeIssue.
+    Since BitBucket does not have an issue API, always returns an empty list.
 
     Args:
-      googlecode_issue: The Google Code Issue.
+      state: The state of the repository can be either 'open' or 'closed'.
+
+    Returns:
+      An empty list.
     """
-    self._issue = googlecode_issue
-    self._dict = {
-        "assignee": self._issue.GetOwner(),
-        "content": self._issue.GetContent(),
-        "content_updated_on": self._issue.GetContentUpdatedOn(),
-        "created_on": self._issue.GetCreatedOn(),
-        "id": self._issue.GetId(),
-        "kind": self._issue.GetKind(),
-        "priority": self._issue.GetPriority(),
-        "reporter": self._issue.GetAuthor(),
-        "status": self._issue.GetStatus(),
-        "title": self._issue.GetTitle(),
-        "updated_on": self._issue.GetUpdatedOn()
+    return []
+
+  def CreateIssue(self, googlecode_issue):
+    """Creates an issue.
+
+    Args:
+      googlecode_issue: An instance of GoogleCodeIssue
+
+    Returns:
+      The issue number of the new issue.
+
+    Raises:
+      ServiceError: An error occurred creating the issue.
+    """
+    bitbucket_issue = {
+        "assignee": googlecode_issue.GetOwner(),
+        "content": googlecode_issue.GetDescription(),
+        "content_updated_on": googlecode_issue.GetContentUpdatedOn(),
+        "created_on": googlecode_issue.GetCreatedOn(),
+        "id": googlecode_issue.GetId(),
+        "kind": googlecode_issue.GetKind(),
+        "priority": googlecode_issue.GetPriority(),
+        "reporter": googlecode_issue.GetAuthor(),
+        "status": googlecode_issue.GetStatus(),
+        "title": googlecode_issue.GetTitle(),
+        "updated_on": googlecode_issue.GetUpdatedOn()
     }
+    self._bitbucket_issues.append(bitbucket_issue)
+    return googlecode_issue.GetId()
 
-  def SetAssignee(self, assignee):
-    self._dict["assignee"] = assignee
-
-  def SetReporter(self, reporter):
-    self._dict["reporter"] = reporter
-
-  def ToDict(self):
-    return self._dict
-
-
-class BitBucketComment(object):
-  """BitBucket comment.
-
-  Handles creating and updating BitBucket comments.
-  """
-
-  def __init__(self, googlecode_comment):
-    """Initialize the BitBucketComment from a GoogleCodeComment.
+  def CloseIssue(self, issue_number):
+    """Closes an issue.
 
     Args:
-      googlecode_comment: The Google Code Comment.
+      issue_number: The issue number.
     """
-    self._comment = googlecode_comment
-    self._dict = {
-        "content": self._comment.GetContent(),
-        "created_on": self._comment.GetCreatedOn(),
-        "id": self._comment.GetId(),
-        "issue": self._comment.GetIssueId(),
-        "updated_on": self._comment.GetUpdatedOn(),
-        "user": self._comment.GetAuthor()
+
+  def CreateComment(self, issue_number, source_issue_id,
+                    googlecode_comment, project_name):
+    """Creates a comment on an issue.
+
+    Args:
+      issue_number: The issue number.
+      source_issue_id: The Google Code issue id.
+      googlecode_comment: An instance of GoogleCodeComment
+      project_name: The Google Code project name.
+    """
+    bitbucket_comment = {
+        "content": googlecode_comment.GetDescription(),
+        "created_on": googlecode_comment.GetCreatedOn(),
+        "id": googlecode_comment.GetId(),
+        "issue": googlecode_comment.GetIssue().GetId(),
+        "updated_on": googlecode_comment.GetUpdatedOn(),
+        "user": googlecode_comment.GetAuthor()
     }
+    self._bitbucket_comments.append(bitbucket_comment)
 
-  def ToDict(self):
-    return self._dict
-
-  def SetUser(self, user):
-    self._dict["user"] = user
-
-
-class IssueConverter(object):
-  """Issue Converter.
-
-  Handles the issues format conversion from Google Code to BitBucket.
-  """
-
-  def __init__(self, issue_json_data, assignee_data, default_issue_kind="bug"):
-    """Initialize the IssuesConverter.
-
-    Args:
-      issue_json_data: The JSON file from Google Takeout
-      assignee_data: The generated JSON file from generate_user_map.py
-      default_issue_kind: Default issue kind (initially set to bug)
+  def WriteIssueData(self, default_issue_kind):
+    """Writes out the json issue and comments data to db-1.0.json.
     """
-    self._issue_data = issue_json_data
-    self._default_issue_kind = default_issue_kind
-    self._assignee_data = assignee_data
-
-  def Convert(self):
-    """The primary function that runs this script.
-    """
-    issues_data = self.CreateBitBucketIssues()
-
+    issues_data = {
+        "issues": self._bitbucket_issues,
+        "comments": self._bitbucket_comments,
+        "meta": {
+            "default_kind": default_issue_kind
+        }
+    }
     with open("db-1.0.json", "w") as issues_file:
       issues_json = json.dumps(issues_data, sort_keys=True, indent=4,
                                separators=(",", ": "), ensure_ascii=False)
       issues_file.write(unicode(issues_json))
 
-  def CreateBitBucketIssues(self):
-    """Create BitBucket issues dict.
 
-    Returns:
-      BitBucket issues dict
-    """
-    bitbucket_issues = []
-    bitbucket_comments = []
+def ExportIssues(issue_file_path, project_name,
+                 user_file_path, default_issue_kind, default_username):
+  """Exports all issues for a given project.
+  """
+  issue_service = IssueService()
+  user_service = UserService()
 
-    for issue in self._issue_data:
-      # Extract Google Code issue from JSON data
-      googlecode_issue = GoogleCodeIssue(issue)
+  issue_data = issues.LoadIssueData(issue_file_path, project_name)
+  user_map = issues.LoadUserData(user_file_path, default_username, user_service)
 
-      # Convert to BitBucket issue
-      bitbucket_issue = BitBucketIssue(googlecode_issue)
+  issue_exporter = issues.IssueExporter(
+      issue_service, user_service, issue_data, project_name, user_map)
 
-      # Set assignee to appropriate BitBucket user
-      bitbucket_issue_assignee = self._assignee_data[
-          googlecode_issue.GetOwner()]
-      bitbucket_issue.SetAssignee(bitbucket_issue_assignee)
-
-      # Set reporter to appropriate BitBucket user
-      bitbucket_issue_reporter = self._assignee_data[
-          googlecode_issue.GetAuthor()]
-      bitbucket_issue.SetReporter(bitbucket_issue_reporter)
-
-      # Append issue to JSON
-      bitbucket_issues.append(bitbucket_issue.ToDict())
-
-      # Extract Google Code comments, create equivalent BitBucket comments,
-      # and append to BitBucket comments dict
-      googlecode_comments = googlecode_issue.GetComments()
-      bitbucket_comments_data = self.CreateBitBucketComments(
-          googlecode_issue.GetId(), googlecode_comments)
-      bitbucket_comments.append(bitbucket_comments_data)
-
-    return {
-        "issues": bitbucket_issues,
-        "comments": bitbucket_comments,
-        "meta": {
-            "default_kind": self._default_issue_kind
-        }
-    }
-
-  def CreateBitBucketComments(self, issue_id, issue_comments):
-    """Create BitBucket comments dict for a particular issue.
-
-    Args:
-      issue_id: Issue ID
-      issue_comments: Comments associated with specified issue
-
-    Returns:
-      BitBucket comments dict for an issue
-    """
-    bitbucket_comments = []
-    for comment in issue_comments:
-      # Extract Google Code comment from JSON data
-      googlecode_comment = GoogleCodeComment(
-          comment, issue_id)
-
-      # Convert to BitBucket comment
-      bitbucket_comment = BitBucketComment(googlecode_comment)
-
-      # Set user to appropriate BitBucket user
-      bitbucket_comment_user = self._assignee_data[
-          googlecode_comment.GetAuthor()]
-      bitbucket_comment.SetUser(bitbucket_comment_user)
-
-      # Append comment to JSON
-      bitbucket_comments.append(bitbucket_comment.ToDict())
-    return bitbucket_comments
+  try:
+    issue_exporter.Init()
+    issue_exporter.Start()
+    issue_service.WriteIssueData(default_issue_kind)
+    print "\nDone!\n"
+  except IOError, e:
+    print "[IOError] ERROR: %s" % e
+  except issues.InvalidUserError, e:
+    print "[InvalidUserError] ERROR: %s" % e
 
 
 def main(args):
@@ -414,45 +173,22 @@ def main(args):
   parser.add_argument("--project_name", required=True,
                       help="The name of the Google Code project you wish to"
                       "export")
-  parser.add_argument("--assignee_file_path", required=True,
+  parser.add_argument("--user_file_path", required=True,
                       help="The path to the file containing a mapping from"
-                      "email address to github username")
+                      "email address to bitbucket username")
   parser.add_argument("--default_issue_kind", required=False,
                       help="A non-null string containing one of the following"
                       "values: bug, enhancement, proposal, task. Defaults to"
                       "bug.")
-  parsed_args, unused_unknown_args = parser.parse_known_args(args)
+  parser.add_argument("--default_owner_username", required=True,
+                      help="The default issue username")
+  parsed_args, _ = parser.parse_known_args(args)
 
-  assignee_data = None
-  issue_data = None
-  issue_converter = None
+  ExportIssues(
+    parsed_args.issue_file_path, parsed_args.project_name,
+    parsed_args.user_file_path, parsed_args.default_issue_kind,
+    parsed_args.default_username)
 
-  # Load user data from Takeout JSON
-  user_file = open(parsed_args.issue_file_path)
-  user_data = json.load(user_file)
-
-  # Load issue data from target project (if exists)
-  user_projects = user_data["projects"]
-  for project in user_projects:
-    if parsed_args.project_name in project["name"]:
-      issue_data = project["items"]
-      break
-
-  if issue_data is None:
-    raise ProjectNotFoundError(
-        "Project %s not found" % parsed_args.project_name)
-
-  # Load assignee data from generated user mapping JSON
-  assignee_file = open(parsed_args.assignee_file_path)
-  assignee_data = json.load(assignee_file)["users"]
-
-  # Set default issue kind (defaults to 'bug')
-  default_issue_kind = parsed_args.default_issue_kind
-
-  issue_converter = IssueConverter(
-      issue_data, assignee_data, default_issue_kind)
-  issue_converter.Convert()
-  print "\nDone!\n"
 
 if __name__ == "__main__":
   main(sys.argv)
---
+++ googlecode-issues-exporter/bitbucket_issue_converter_test.py
@@ -0,0 +1,189 @@
+# Copyright 2013 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for the BitBucket Services."""
+
+# pylint: disable=missing-docstring,protected-access
+
+import unittest
+
+import bitbucket_issue_converter
+import issues
+
+from issues_test import DEFAULT_USERNAME
+from issues_test import SINGLE_COMMENT
+from issues_test import SINGLE_ISSUE
+from issues_test import COMMENT_ONE
+from issues_test import COMMENT_TWO
+from issues_test import COMMENT_THREE
+from issues_test import COMMENTS_DATA
+from issues_test import NO_ISSUE_DATA
+from issues_test import USER_MAP
+
+
+# The BitBucket username.
+BITBUCKET_USERNAME = DEFAULT_USERNAME
+# The BitBucket repo name.
+BITBUCKET_REPO = "repo"
+
+
+class TestUserService(unittest.TestCase):
+  """Tests for the UserService."""
+
+  def setUp(self):
+    self._bitbucket_user_service = bitbucket_issue_converter.UserService()
+
+  def testIsUser123(self):
+    is_user = self._bitbucket_user_service.IsUser("username123")
+    self.assertTrue(is_user)
+
+  def testIsUser321(self):
+    is_user = self._bitbucket_user_service.IsUser("username321")
+    self.assertTrue(is_user)
+
+
+class TestIssueService(unittest.TestCase):
+  """Tests for the IssueService."""
+
+  def setUp(self):
+    self._bitbucket_issue_service = bitbucket_issue_converter.IssueService()
+    self.maxDiff = None
+
+  def testCreateIssue(self):
+    issue_body = {
+        "assignee": "a_uthor",
+        "content": ("Original [issue 1](https://code.google.com/p/repo/issues" +
+                 "/detail?id=1) created by a_uthor on last year:\n\none"),
+        "content_updated_on": "last month",
+        "created_on": "last year",
+        "id": 1,
+        "kind": "Defect",
+        "priority": "Medium",
+        "reporter": None,
+        "status": "fixed",
+        "title": "issue_title",
+        "updated_on": "last year",
+    }
+    issue_number = self._bitbucket_issue_service.CreateIssue(SINGLE_ISSUE)
+    self.assertEqual(1, issue_number)
+    actual = self._bitbucket_issue_service._bitbucket_issues[0]
+    self.assertEqual(issue_body, actual)
+
+  def testCloseIssue(self):
+    # no-op
+    self._bitbucket_issue_service.CloseIssue(123)
+
+  def testCreateComment(self):
+    comment_body = {
+        "content": (
+            "Comment [#1](https://code.google.com/p/repo/issues/detail" +
+            "?id=1#c1) originally posted by a_uthor on last year:\n\none"),
+        "created_on": "last year",
+        "id": 1,
+        "issue": 1,
+        "updated_on": "last year",
+        "user": "a_uthor",
+    }
+    self._bitbucket_issue_service.CreateComment(
+        1, "1", SINGLE_COMMENT, BITBUCKET_REPO)
+    actual = self._bitbucket_issue_service._bitbucket_comments[0]
+    self.assertEqual(comment_body, actual)
+
+
+class TestIssueExporter(unittest.TestCase):
+  """Tests for the IssueService."""
+
+  def setUp(self):
+    self._bitbucket_user_service = bitbucket_issue_converter.UserService()
+    self._bitbucket_issue_service = bitbucket_issue_converter.IssueService()
+    self.issue_exporter = issues.IssueExporter(
+        self._bitbucket_issue_service, self._bitbucket_user_service,
+        NO_ISSUE_DATA, BITBUCKET_REPO, USER_MAP)
+    self.issue_exporter.Init()
+
+  def testGetAllPreviousIssues(self):
+    self.assertEqual(0, len(self.issue_exporter._previously_created_issues))
+    self.issue_exporter._GetAllPreviousIssues()
+    self.assertEqual(0, len(self.issue_exporter._previously_created_issues))
+
+  def testCreateIssue(self):
+    issue_number = self.issue_exporter._CreateIssue(SINGLE_ISSUE)
+    self.assertEqual(1, issue_number)
+
+  def testCreateComments(self):
+    self.assertEqual(0, self.issue_exporter._comment_number)
+    self.issue_exporter._CreateComments(COMMENTS_DATA, 1234, SINGLE_ISSUE)
+    self.assertEqual(4, self.issue_exporter._comment_number)
+
+  def testStart(self):
+    self.issue_exporter._issue_json_data = [
+        {
+            "id": "1",
+            "title": "Title1",
+            "state": "open",
+            "status": "New",
+            "comments": {
+                "items": [COMMENT_ONE, COMMENT_TWO, COMMENT_THREE],
+            },
+            "labels": ["Type-Issue", "Priority-High"],
+            "owner": {"kind": "projecthosting#issuePerson",
+                      "name": "User1"
+                     },
+            "published": "last year",
+            "updated": "last month",
+        },
+        {
+            "id": "2",
+            "title": "Title2",
+            "state": "closed",
+            "status": "Fixed",
+            "owner": {"kind": "projecthosting#issuePerson",
+                      "name": "User2"
+                     },
+            "labels": [],
+            "comments": {
+                "items": [COMMENT_ONE],
+            },
+            "published": "last month",
+            "updated": "last week",
+        },
+        {
+            "id": "3",
+            "title": "Title3",
+            "state": "closed",
+            "status": "WontFix",
+            "comments": {
+                "items": [COMMENT_ONE, COMMENT_TWO],
+            },
+            "labels": ["Type-Defect"],
+            "owner": {"kind": "projecthosting#issuePerson",
+                      "name": "User3"
+                     },
+            "published": "last week",
+            "updated": "yesterday",
+        }]
+
+    self.issue_exporter.Start()
+
+    self.assertEqual(3, self.issue_exporter._issue_total)
+    self.assertEqual(3, self.issue_exporter._issue_number)
+    # Comment counts are per issue and should match the numbers from the last
+    # issue created, minus one for the first comment, which is really
+    # the issue description.
+    self.assertEqual(1, self.issue_exporter._comment_number)
+    self.assertEqual(1, self.issue_exporter._comment_total)
+
+
+if __name__ == "__main__":
+  unittest.main(buffer=True)
